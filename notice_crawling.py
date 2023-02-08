a89d9c2233e6504a946094163be9f5ba61117353
yungoon's pi <sirius584@naver.com>
2023-02-09 00:52:43 +0900

    good

--- notice_crawling.py
+++ notice_crawling.py
@@ -2,11 +2,12 @@ import requests
 import time
 import telegram
 from bs4 import BeautifulSoup
-from datetime import date
+from datetime import date, timedelta
 import logging
 import pandas
 import lxml
 import asyncio
+import schedule
 
 headers = {
     "Connection": "keep-alive",
@@ -33,28 +34,38 @@ url = 'https://www.kw.ac.kr/ko/life/notice.jsp?srCategoryId=&mode=list&searchKey
 bot = telegram.Bot(token=TOKEN)
 
 
+def main():
+    # requests, bs4
+    res = requests.get(url, headers=headers)
+    res.raise_for_status() #에러가 났으면 멈추고, 에러를 알려줌
+    soup = BeautifulSoup(res.text, "lxml") 
 
-# requests, bs4
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
+    # 공지사항 목록 받기
+    to_find_notice = soup.find_all("li", {'class' : 'top-notice'})
+    #오늘 날짜를 문자열로
+    today = str(date.today())
+    yesterday = str(date.today() - timedelta(days=1))
+        # 공지사항 전송
+    for notice in to_find_notice:
+        post_date = notice.p.get_text().split()[4] #7 : 수정일 4 : 게시일
+        if(today == post_date or post_date == yesterday): #오늘, 어제 게시글이 있으면
+            title = notice.a.get_text().split('\n')[1]
+            link = 'https://www.kw.ac.kr' + \
+                notice.a['href']
+            feed = '{0}\n\n게시일: {1}\n\n링크: {2}'.format(title, post_date, link)
+            print(feed,"\n")
+            bot.send_message(text=feed, chat_id=CHAT_ID)
+            time.sleep(2)
+        else:
+            continue
 
-# 공지사항 목록 받기
-to_find_notice = soup.find_all("li", {'class' : 'top-notice'})
+#schedule.every(1).day.at("9:00").do(main) #매일 9시에 실행
+schedule.every(1).minutes.do(main) #매 1분마다 실행
 
-#오늘 날짜를 문자열로
-today = str(date.today())
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
-# 공지사항 전송
-for notice in to_find_notice:
-    post_date = notice.p.get_text().split()[4] #7 : 수정일 4 : 게시일
-    if(today == post_date): #오늘 게시글이 있으면
-        title = notice.a.get_text().split('\n')[1]
-        link = 'https://www.kw.ac.kr' + \
-            notice.a['href']
-        feed = '{0}\n\n게시일: {1}\n\n링크: {2}'.format(title, post_date, link)
-        print(feed,"\n")
-        asyncio.run(bot.send_message(text=feed, chat_id=CHAT_ID))
-        time.sleep(2)
-    else:
-        continue
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(main())
+#loop.close()
--- test.py
+++ test.py
@@ -1,49 +1,71 @@
-from bs4 import BeautifulSoup
-from datetime import date
 import requests
 import time
 import telegram
+from bs4 import BeautifulSoup
+from datetime import date
+import logging
+import pandas
+import lxml
+import asyncio
+import schedule
+
+headers = {
+    "Connection": "keep-alive",
+    "Cache-Control": "max-age=0",
+    "sec-ch-ua-mobile": "?0",
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "document",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "ko-KR,ko;q=0.9"
+}
 
 # Fixed values
-TOKEN = '텔레그램에서 발급받은 토큰'
-CHAT_ID = '메세지를 전송할 채팅방 ID'
-url = "https://teaching.korea.ac.kr/teaching/community/notice1.do"
+TOKEN = '6023105153:AAFu0pruC11fYbC-b-ZQQ_gMgVjrExG4oH8'
+CHAT_ID = '-1001877056305'
+url = 'https://www.kw.ac.kr/ko/life/notice.jsp?srCategoryId=&mode=list&searchKey=1&searchVal='
 
 # Telegram bot
-# bot = telegram.Bot(token=TOKEN)
-
-# requests, bs4
-res = requests.get(url)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "html.parser")
+bot = telegram.Bot(token=TOKEN)
 
 
-# 클래스 구분이 명확하지 않은 페이지에서 공지사항 리스트(ul) 추출(사이트 구조 변경 시 수정 필요)
-to_find_notice = soup.findAll("ul", attrs={"class": "m"})
-grabed_notice = to_find_notice[1]
+def main():
+    # requests, bs4
+    res = requests.get(url, headers=headers)
+    res.raise_for_status() #에러가 났으면 멈추고, 에러를 알려줌
+    soup = BeautifulSoup(res.text, "lxml") 
 
-#print(to_find_notice)
-#print(grabed_notice)
+    # 공지사항 목록 받기
+    to_find_notice = soup.find_all("li", {'class' : 'top-notice'})
+    #오늘 날짜를 문자열로
+    today = str(date.today())
+    yesterday = str(date.today() - timedelta(days=1))
+        # 공지사항 전송
+    for notice in to_find_notice:
+        post_date = notice.p.get_text().split()[4] #7 : 수정일 4 : 게시일
+        if(today == post_date or post_date == yesterday): #오늘, 어제 게시글이 있으면
+            title = notice.a.get_text().split('\n')[1]
+            link = 'https://www.kw.ac.kr' + \
+                notice.a['href']
+            feed = '{0}\n\n게시일: {1}\n\n링크: {2}'.format(title, post_date, link)
+            logging(feed,"\n")
+            bot.send_message(text=feed, chat_id=CHAT_ID)
+            time.sleep(2)
+        else:
+            continue
 
-# 각 공지사항의 element에 접근하기 위해 공지사항 li elements를 리스트 자료구조로 변환
-notice_list = grabed_notice.findAll('li')
-#print(notice_list)
+#schedule.every(1).day.at("9:00").do(main) #매일 9시에 실행
+schedule.every(1).minutes.do(main) #매 1분마다 실행
 
-#오늘 날짜를 문자열로
-today = date.today().isoformat().replace('-', '.')
-print(type(today))
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
-# 공지사항 전송
-for notice in notice_list:
-    post_date = notice.span.get_text().replace('교직팀', '')
-    #print(notice)
-    if(today == post_date): #오늘 게시글이 있으면
-        title = notice.a.get_text()
-        link = 'https://teaching.korea.ac.kr/teaching/community/notice1.do' + \
-            notice.a['href']
-        feed = '{0}\n\n게시일: {1}\n\n링크: {2}'.format(title, post_date, link)
-        #print(feed)
-        #bot.send_message(text=feed, chat_id=CHAT_ID)
-        time.sleep(2)
-    else:
-        continue
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(main())
+#loop.close()
