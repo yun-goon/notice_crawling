2865d45ad49716119bf81e91057e3a6aa132c7ef
yungoon's pi <sirius584@naver.com>
2023-02-09 21:03:03 +0900

    notice_crawling

--- notice_crawling.py
+++ notice_crawling.py
@@ -1,13 +1,15 @@
 import requests
+from datetime import datetime
 import time
 import telegram
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
 import logging
-import pandas
 import lxml
-import asyncio
 import schedule
+import privacy as pr
+#import pandas
+#import asyncio
 
 headers = {
     "Connection": "keep-alive",
@@ -26,13 +28,14 @@ headers = {
 }
 
 # Fixed values
-TOKEN = '6023105153:AAFu0pruC11fYbC-b-ZQQ_gMgVjrExG4oH8'
-CHAT_ID = '-1001877056305'
+TOKEN = pr.TOKEN
+CHAT_ID = pr.CHAT_ID
 url = 'https://www.kw.ac.kr/ko/life/notice.jsp?srCategoryId=&mode=list&searchKey=1&searchVal='
 
 # Telegram bot
 bot = telegram.Bot(token=TOKEN)
 
+list = []
 
 def main():
     # requests, bs4
@@ -45,11 +48,17 @@ def main():
     #오늘 날짜를 문자열로
     today = str(date.today())
     yesterday = str(date.today() - timedelta(days=1))
-        # 공지사항 전송
+    #now = datetime.now()
+
+    # 공지사항 전송
     for notice in to_find_notice:
-        post_date = notice.p.get_text().split()[4] #7 : 수정일 4 : 게시일
+        post_date = notice.p.get_text().split()[4] #7 : 수정일 4 : 게시일 7
         if(today == post_date or post_date == yesterday): #오늘, 어제 게시글이 있으면
             title = notice.a.get_text().split('\n')[1]
+            if (list.count(title) > 0): #list에 존재하면 넘어가고 없으면 추가.
+                continue
+            else:
+                list.append(title)
             link = 'https://www.kw.ac.kr' + \
                 notice.a['href']
             feed = '{0}\n\n게시일: {1}\n\n링크: {2}'.format(title, post_date, link)
@@ -59,13 +68,19 @@ def main():
         else:
             continue
 
-#schedule.every(1).day.at("9:00").do(main) #매일 9시에 실행
-schedule.every(1).minutes.do(main) #매 1분마다 실행
+def list_clear():
+    list.clear()
+
+schedule.every(1).hour.do(main) #1시간마다 검사 
+schedule.every(1).day.at("12:00").do(main) #매일 한번씩 12시에 list 초기화
+#job1 = schedule.every(1).minutes.do(main) #매 1분마다 실행
 
 while True:
     schedule.run_pending()
     time.sleep(1)
 
+
+#비동기...?
 #loop = asyncio.get_event_loop()
 #loop.run_until_complete(main())
 #loop.close()
